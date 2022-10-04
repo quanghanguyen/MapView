061852d0af17c7a849180d877477a1be0b780e8b
QuangHa <duongthien.mcs@gmail.com>
2022-10-04 10:24:21 +0700

    0410

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let map = MKMapView()
+    var location: CLLocationManager?
+    private var longtitude : Double?
+    private var latitude : Double?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getLocation()
+        addView()
+    }
+    
+    private func getLocation() {
+        location = CLLocationManager()
+        location?.delegate = self
+        location?.requestAlwaysAuthorization()
+        location?.startUpdatingLocation()
+    }
+    
+    private func addView() {
+        addMap()
+    }
+    
+    private func addMap() {
+        view.addSubview(map)
+        map.frame = view.bounds
     }
 
+    
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            longtitude = location.coordinate.longitude
+            latitude = location.coordinate.latitude
+        }
+        let pick = CLLocationCoordinate2D(latitude: latitude ?? 0, longitude: longtitude ?? 0)
+        map.setRegion(MKCoordinateRegion(center: pick, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)), animated: true)
+    }
 }
 
